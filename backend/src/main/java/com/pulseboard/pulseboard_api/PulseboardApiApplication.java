b3768879226119de94e60bb0ed05b764c66ee9d5
DhananjayaKularatne <djaya2100@gmail.com>
2026-07-11 21:30:40 +0530

    chore: initialize Spring Boot project skeleton

---
+++ backend/src/main/java/com/pulseboard/pulseboard_api/PulseboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.pulseboard.pulseboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseboardApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pulseboard/pulseboard_api/PulseboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulseboard.pulseboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
